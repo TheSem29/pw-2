cb54e311423ec63f85205e41422056e563a2ce59
TheSem29 <semsemsem1541@gmail.com>
2025-12-05 18:03:03 +0300

    Add files via upload

--- pw_2.py
+++ pw_2.py
@@ -1,2 +1,14 @@
-a = 1,2,3
-print(type(a))
+class Car:
+    def __init__(self, name, brand, max_speed, release_year):
+        self.name = name
+        self.brand = brand
+        self.release_year = release_year
+        self.max_speed = max_speed
+
+    def calculate_premium()
+
+
+MarkII = Car('MarkII', 'Toyota', 175, 1968)
+MarkII_III = Car('MarkII_III', 'Toyota', 190, 1976)
+
+print(MarkII.get_car_age)
