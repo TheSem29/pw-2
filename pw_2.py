624e356bed0f7e95b3aac54dbe9f3d140a0d42d9
TheSem29 <semsemsem1541@gmail.com>
2025-12-12 15:01:59 +0300

    Add files via upload

--- pw_2.py
+++ pw_2.py
@@ -1,14 +1,40 @@
 class Car:
-    def __init__(self, name, brand, max_speed, release_year):
-        self.name = name
-        self.brand = brand
-        self.release_year = release_year
-        self.max_speed = max_speed
+    def __init__(self, license_plate, year, horsepower):
+        self.license_plate = license_plate
+        self.year = year
+        self.horsepower = horsepower
 
-    def calculate_premium()
+    def get_age(self):
+        return 2025 - self.year
+
+    def _get_power_coeff(self):
+        if self.horsepower <= 70:
+            return 0.8
+        elif 71 <= self.horsepower <= 100:
+            return 1.0
+        elif 101 <= self.horsepower <= 150:
+            return 1.2
+        else:
+            return 1.5
+
+    def _get_age_coeff(self):
+        age = self.get_age()
+        if age <= 3:
+            return 0.9
+        elif 4 <= age <= 7:
+            return 1.0
+        elif 8 <= age <= 10:
+            return 1.1
+        else:
+            return 1.3
+
+    def calculate_premium(self, base_tariff=4000):
+        km = self._get_power_coeff()
+        kva = self._get_age_coeff()
+        return base_tariff * km * kva
 
 
-MarkII = Car('MarkII', 'Toyota', 175, 1968)
-MarkII_III = Car('MarkII_III', 'Toyota', 190, 1976)
 
-print(MarkII.get_car_age)
+car = Car("A123BC77", 2018, 110)
+
+print(f"Стоимость полиса: {car.calculate_premium():.2f} руб.")
